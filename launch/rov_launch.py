2851a001532f2c8a8b6eb747276a83ae18d661e9
Lucas Niewohner <lucas@birdseyerobotics.com>
2022-04-20 21:04:17 -0500

    WIP implementing joystick control in rov_control

--- launch/rov_launch.py
+++ launch/rov_launch.py
@@ -1,24 +1,23 @@
-# import os
-# from ament_index_python.packages import get_package_share_directory
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+
 # import re
 # import subprocess
+
 
 def generate_launch_description():
     ld = LaunchDescription()
 
-    # robot_control_config = os.path.join(
-    #     get_package_share_directory("robot_control"), "config", "default.yaml"
-    # )
+    rov_control_config = os.path.join(get_package_share_directory("rov_control"), "config", "default.yaml")
 
     ld.add_action(
         Node(
-            package="robot_control",
-            executable="robot_control",
-            name="robot_control",
-            # parameters=[robot_control_config, override_config],
-            # remappings=[("/robot/cmd_velocity", "/odrive/cmd_velocity")],
+            package="rov_control",
+            executable="rov_control",
+            name="rov_control",
+            parameters=[rov_control_config],
         )
     )
 
--- src/rov/rov_control/rov_control/rov_control.py
+++ src/rov/rov_control/rov_control/rov_control.py
@@ -1,3 +1,4 @@
+from typing import Dict
 import rclpy
 from rclpy.node import Node
 
@@ -9,64 +10,162 @@ from std_msgs.msg import String
 
 from enum import Enum
 
-class Joystick_Map_Axes(Enum):
-    # AXES
-    PITCH = 0
-    YAW = 1
-    ROLL = 2
-    THROTTLE = 3
 
-class Joystick_Map_Buttons(Enum):  
-    # BUTTONS
-    INTERACT = 0
-    MODE_SWITCH = 1
+class Buttons:
+    def __init__(self):
+        self.trigger = False
+        self.thumb = False
+        self.button_3 = False
+        self.button_4 = False
+        self.button_5 = False
+        self.button_6 = False
+        self.button_7 = False
+        self.button_8 = False
+        self.button_9 = False
+        self.button_10 = False
+        self.button_11 = False
+        self.button_12 = False
+        self.hat_up = False
+        self.hat_down = False
+        self.hat_left = False
+        self.hat_right = False
+
+
+class ParsedJoy(Buttons):
+    last_buttons: Buttons = Buttons()
+
+    def __init__(self, msg: Joy, mapping: Dict[str, str]):
+        super().__init__()
+        self.trigger = bool(msg.buttons[mapping["trigger"]])
+        self.thumb = bool(msg.buttons[mapping["thumb"]])
+        self.button_3 = bool(msg.buttons[mapping["button_3"]])
+        self.button_4 = bool(msg.buttons[mapping["button_4"]])
+        self.button_5 = bool(msg.buttons[mapping["button_5"]])
+        self.button_6 = bool(msg.buttons[mapping["button_6"]])
+        self.button_7 = bool(msg.buttons[mapping["button_7"]])
+        self.button_8 = bool(msg.buttons[mapping["button_8"]])
+        self.button_9 = bool(msg.buttons[mapping["button_9"]])
+        self.button_10 = bool(msg.buttons[mapping["button_10"]])
+        self.button_11 = bool(msg.buttons[mapping["button_11"]])
+        self.button_12 = bool(msg.buttons[mapping["button_12"]])
+        # Parse the button pushes for the dpad
+        self.hat_up = msg.axes[mapping["hat_y"]] > 0
+        self.hat_down = msg.axes[mapping["hat_y"]] < 0
+        self.hat_left = msg.axes[mapping["hat_x"]] < 0
+        self.hat_right = msg.axes[mapping["hat_x"]] > 0
+        self.toggledButtons = self.get_toggled()
+        self.roll = msg.axes[mapping["roll"]]
+        self.pitch = msg.axes[mapping["pitch"]]
+        self.yaw = msg.axes[mapping["yaw"]]
+        self.throttle = msg.axes[mapping["throttle"]]
+        self.hat_x = msg.axes[mapping["hat_x"]]
+        self.hat_y = msg.axes[mapping["hat_y"]]
+
+    def get_toggled(self):
+        toggled_buttons = Buttons()
+        for button in dir(toggled_buttons):
+            # Here, we raise our middle fingers to the Python gods in our
+            # religious pursuit of avoiding code duplication, and pray that
+            # their retribution is mild
+            if callable(getattr(toggled_buttons, button)):
+                continue
+            if getattr(self, button) and not getattr(self.last_buttons, button):
+                setattr(
+                    toggled_buttons,
+                    button,
+                    # Set this button to "Toggled" if
+                    # A) it's pushed now and
+                    # B) it wasn't pushed before
+                    (getattr(self, button) and not getattr(self.last_buttons, button)),
+                )
+        self.last_buttons = self
+        return toggled_buttons
+
 
 class States(Enum):
     shutdown = 0
     paused = 1
     teleop_drive = 2
     teleop_manip = 3
-    
+
+
 class ROV_Control(Node):
     def __init__(self):
         super().__init__("rov_control")
-
         self.thruster_setpoint_publisher = self.create_publisher(ThrusterSetpoints, "thruster_setpoints", 10)
         self.machine = Machine(self, states=States, initial=States.teleop_drive)
         self.machine.add_transition("estop_normal", "*", States.paused)
         self.machine.add_transition("estop_fatal", "*", States.shutdown)
         self.machine.add_transition("teleop_mode_switch", States.teleop_manip, States.teleop_drive)
         self.machine.add_transition("teleop_mode_switch", States.teleop_drive, States.teleop_manip)
+        self.mapping = self.dictionaryafy(self.get_parameters_by_prefix("mapping"))
+        self.last_manip = ManipulatorSetpoints(
+            clamp=self.get_parameter("clamp_initial").value,
+            level=self.get_parameter("level_initial").value,
+            wrist=self.get_parameter("wrist_initial").value,
+            elbow=self.get_parameter("elbow_initial").value,
+        )
+        self.last_joy_time = 0
         self.joy_subscription = self.create_subscription(Joy, "joy", self.joystick_callback, 10)
-        self.joystick = Joy()
-        self.manipulator_setpoint_publisher = self.create_publisher(ManipulatorSetpoints, "manipulator_setpoints", 10)
+        self.manipulator_setpoint_pub = self.create_publisher(ManipulatorSetpoints, "manipulator_setpoints", 2)
+        self.thruster_setpoint_pub = self.create_publisher(ThrusterSetpoints, "thruster_setpoints", 2)
         self.state_pub = self.create_publisher(String)
-        self.create_timer(5, lambda: self.state_pub.publish())
+        # self.create_timer(5, lambda: self.state_pub.publish())
         self.create_subscription(Bool, "estop", self.estop_callback, 0)
-        
 
-    def joystick_callback(self, msg:Joy):
-        self.joystick=msg
+    def dictionaryafy(self, in_dict):
+        tree = {}
+        for key, value in in_dict.items():
+            t = tree
+            parts = key.split(".")
+            for part in parts[:-1]:
+                t = t.setdefault(part, {})
+            t[parts[-1]] = value.value
+        return tree
+
+    def joystick_callback(self, msg: Joy):
+        # self.joystick = ParsedJoy(msg)
+        joystick = ParsedJoy(msg, self.mapping)
+
+        current_sec = self.get_clock().now().nanoseconds / 1000000.0
+        if self.last_joy_time == 0:
+            delta_time = 0
+        else:
+            delta_time = min(current_sec - self.last_joy_time, 0.2)
+        self.last_joy_time = current_sec
 
         # if mode switch button has been pressed, toggle the mode if you are in teleop
-        if(self.state in [States.teleop_manip, States.teleop_drive]):
-            buttons = self.joystick.buttons
-            if(buttons[Joystick_Map_Buttons.MODE_SWITCH]):
+        if self.state in [States.teleop_manip, States.teleop_drive]:
+            if joystick.button_8:
                 self.teleop_mode_switch()
 
         # update teleop modes
-        if(self.state == States.teleop_manip):
-            self.do_manip_setpoint_update()
-        elif(self.state == States.teleop_drive):
-            self.do_thrust_setpoint_update()
-    
-    def do_manip_setpoint_update(self):
-        axes = self.joystick.axes
-        manip_setpoints = ManipulatorSetpoints()
-        
-    def do_thrust_setpoint_update(self):
-        axes = self.joystick.axes
-        thrust_setpoints = ThrusterSetpoints()
+        if self.state == States.teleop_manip:
+            self.do_manip_setpoint_update(joystick, delta_time)
+        elif self.state == States.teleop_drive:
+            self.do_thrust_setpoint_update(joystick)
+
+    def do_manip_setpoint_update(self, joystick: ParsedJoy, delta_time: float):
+        manip_setpoints = self.last_manip
+        chicken_speed = self.get_parameter("chicken_speed").value
+        manip_setpoints.elbow += joystick.pitch * chicken_speed * delta_time
+        # chicken
+        if joystick.thumb:
+            # Do IK here :)
+            manip_setpoints.level = 180 - manip_setpoints.elbow
+        else:
+            manip_setpoints.level += joystick.hat_y * chicken_speed * delta_time
+
+        if joystick.toggledButtons.button_3:
+            manip_setpoints.clamp = not manip_setpoints.clamp
+
+        manip_setpoints.wrist += joystick.roll * self.get_parameter("wrist_speed").value * delta_time
+
+        self.manipulator_setpoint_pub.publish(manip_setpoints)
+        self.last_manip = manip_setpoints
+
+    def do_thrust_setpoint_update(self, joystick: ParsedJoy):
+        pass
 
     def estop_callback(self, msg):
         if msg.data:
@@ -84,10 +183,12 @@ class ROV_Control(Node):
         self.get_logger().fatal("ROV_Control has entered emergency shutdown")
         pass
 
+
 def main():
     rclpy.init(args=None)
     rclpy.spin(ROV_Control())
     rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
