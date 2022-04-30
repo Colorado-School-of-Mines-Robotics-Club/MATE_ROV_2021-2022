45b6736d1cfb8993d459ae6447dd96ba780dd541
Zac Stanton <zac71113@gmail.com>
2022-04-30 15:20:31 -0600

    we are using nanogui now :)
    Co-authored-by: PhocaeaUnitas <PhocaeaUnitas@users.noreply.github.com>

--- src/basestation/bs_gui/src/bs_gui.cpp
+++ src/basestation/bs_gui/src/bs_gui.cpp
@@ -14,15 +14,19 @@
 #include <compressed_image_transport/compression_common.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/msg/compressed_image.hpp>
+#include <sensor_msgs/msg/joy.hpp>
 #include <std_msgs/msg/bool.hpp>
 
 #include <opencv2/opencv.hpp>
 
-#include <SDL.h>
+#include <nanogui/nanogui.h>
+
+#include <eigen3/Eigen/Core>
 
 class BS_GUI : public rclcpp::Node {
 public:
     BS_GUI() : Node("bs_gui") {
+        std::cout << "we constructing" << std::endl;
         // camera streams
         // TODO: consider making these multithreaded
         std::function<void(const sensor_msgs::msg::CompressedImage::SharedPtr)> callback0 = std::bind(&BS_GUI::camera_callback, this, 0, std::placeholders::_1);
@@ -39,6 +43,10 @@ public:
         camera_control_publishers.emplace(std::make_pair(1, this->create_publisher<std_msgs::msg::Bool>("cam1_control", 10)));
         camera_control_publishers.emplace(std::make_pair(2, this->create_publisher<std_msgs::msg::Bool>("cam2_control", 10)));
         camera_control_publishers.emplace(std::make_pair(3, this->create_publisher<std_msgs::msg::Bool>("cam3_control", 10)));
+
+        // monitor joystick
+        std::function<void(const sensor_msgs::msg::Joy::SharedPtr)> joy_callback = std::bind(&BS_GUI::joystick_callback, this, std::placeholders::_1);
+        joystick_subscription = this->create_subscription<sensor_msgs::msg::Joy>("joy", 10, joy_callback);
 
         // texture streams for changing buffers
         // this->cam0_image = std::make_shared<SDL_Texture>(SDL_CreateTexture(renderer.get(), SDL_PIXELFORMAT_BGR888, SDL_TEXTUREACCESS_STREAMING, 320, 240));
@@ -60,13 +68,38 @@ private:
 
         //todo: display image to screen
     }
+
+    void joystick_callback(const sensor_msgs::msg::Joy::SharedPtr msg) {
+        *this->joystick.get() = *msg.get();
+    }
+
     std::unordered_map<int, std::shared_ptr<rclcpp::Subscription<sensor_msgs::msg::CompressedImage>>> camera_image_subscriptions;
     std::unordered_map<int, std::shared_ptr<rclcpp::Publisher<std_msgs::msg::Bool>>> camera_control_publishers;
+    std::shared_ptr<rclcpp::Subscription<sensor_msgs::msg::Joy>> joystick_subscription;
+
+    std::shared_ptr<sensor_msgs::msg::Joy> joystick;
 };
 
-int main(int argc, char ** argv) {
+void target(int argc, char ** argv) {
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<BS_GUI>());
     rclcpp::shutdown();
+}
+
+
+int main(int argc, char ** argv) {
+    nanogui::init();
+
+    std::shared_ptr<nanogui::Screen> screen = std::make_shared<nanogui::Screen>(nanogui::Vector2i(100,100), "Bruh");
+    // add widgets
+    std::shared_ptr<nanogui::Button> button = std::make_shared<nanogui::Button>(screen.get());
+    screen->setVisible(true);
+    screen->performLayout();
+    
+    // ros thread
+    std::thread t(std::bind(&target, argc, argv));
+
+    // run screen's mainloop
+    nanogui::mainloop();
     return 0;
 }
